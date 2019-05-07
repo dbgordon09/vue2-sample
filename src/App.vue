4eea9eca38d4cf69a314b02cf8e7dedffc658aaa
Dale Gordon <dbgordon09@gmail.com>
2019-05-07 08:13:37 +0200

    Boostrap Vs Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
     <div id="app">
-        <Navbar/>
-        <router-view/>
+        <BulmaNavbar/>
+        <SideDrawer/>
+        <router-view class="mt-5"/>
     </div>
 </template>
 
 <script>
-    import Navbar from './components/layouts/AppNavbar'
+    import BulmaNavbar from "./components/layouts/BulmaNavbar";
+    import SideDrawer from "./components/layouts/SideDrawer";
 
     export default {
         name: 'app',
         components: {
-            Navbar,
+            BulmaNavbar,
+            SideDrawer,
         }
     }
 </script>
---
+++ src/components/layouts/SideDrawer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="sidedrawer" class="navbar-menu is-dark" role="navigation" aria-label="main navigation">
+        <router-link to="/" class="navbar-item">Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SideDrawer',
+    }
+</script>
+<style>
+    #sidedrawer {
+        position: fixed;
+        top: 0;
+        right: 0;
+        overflow-x: hidden;
+
+        min-width: 250px;
+        height: 100%;
+        background-color: #363636;
+        color: #fff;
+        z-index: 1;
+
+    }
+
+</style>
